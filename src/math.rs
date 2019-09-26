49c2e6b35b1eda990a34348a5766e0de4509de2c
PENGUINLIONG <admin@penguinliong.moe>
2019-09-26 13:58:45 -0700

    Improve math module design

--- src/math.rs
+++ src/math.rs
@@ -32,6 +32,15 @@ impl PartialEq for Vector {
 impl Default for Vector {
     fn default() -> Self { unsafe { Vector(_mm_setzero_ps()) } }
 }
+impl Mul<Transform> for Vector {
+    type Output = Vector;
+    fn mul(self, rhs: Transform) -> Vector {
+        unsafe {
+            let c = _mm_or_ps(self.0, _mm_set_ps(1.0, 0.0, 0.0, 0.0));
+            Vector(_mm_or_ps(_mm_or_ps(_mm_dp_ps(rhs.0, c, 0xf1), _mm_dp_ps(rhs.1, c, 0xf2)), _mm_dp_ps(rhs.2, c, 0xf4)))
+        }
+    }
+}
 impl Vector {
     pub fn new(x: f32, y: f32, z: f32) -> Self { unsafe { Vector(_mm_set_ps(0.0, z, y, x)) } }
     pub fn dot(self, rhs: Self) -> f32 { unsafe { _mm_cvtss_f32(_mm_dp_ps(self.0, rhs.0, 0xf1)) } }
@@ -60,12 +69,19 @@ impl PartialEq for Point {
         unsafe { _mm_movemask_ps(_mm_cmpneq_ps(self.0, rhs.0)) == 0x0f }
     }
 }
+impl Sub for Point {
+    type Output = Vector;
+    fn sub(self, rhs: Self) -> Vector { unsafe { Vector(_mm_sub_ps(self.0, rhs.0)) } }
+}
+impl Add<Vector> for Point {
+    type Output = Point;
+    fn add(self, rhs: Vector) -> Point { unsafe { Point(_mm_add_ps(self.0, rhs.0)) } }
+}
 impl Default for Point {
     fn default() -> Self { unsafe { Point(_mm_setzero_ps()) } }
 }
 impl Point {
     pub fn new(x: f32, y: f32, z: f32) -> Self { unsafe { Point(_mm_set_ps(0.0, z, y, x)) } }
-    pub fn affine(self, rhs: Vector) -> Self { unsafe { Point(_mm_add_ps(self.0, rhs.0)) } }
 }
 
 
@@ -96,33 +112,27 @@ impl<T> PointSet for T where T: AsRef<[Point]> {}
 
 
 
-#[derive(Clone, Copy, Debug)]
-pub struct Transform(__m128, __m128, __m128); // Row-major, 3x4 matrix, left-mul for transform.
-impl Mul<Vector> for Transform {
-    type Output = Vector;
-    fn mul(self, rhs: Vector) -> Vector {
-        unsafe {
-            let c = _mm_insert_ps(rhs.0, _mm_set1_ps(1.0), 0x00);
-            Vector(_mm_or_ps(_mm_or_ps(_mm_dp_ps(self.0, c, 0xf1), _mm_dp_ps(self.1, c, 0xf2)), _mm_dp_ps(self.2, c, 0xf4)))
-        }
-    }
+/// 4x4 column-major matrix, this can do no algebra and should be only used as a
+/// data container.
+pub struct Matrix(__m128, __m128, __m128, __m128);
+trait ToMatrix {
+    // Convert a linear operator to Vulkan-compatible column-major matrix,
+    // stored in contigeous memory.
+    fn to_matrix(&self) -> Matrix;
 }
+
+/// 3x4 row-major matrix, right-mul to apply transform. Less memory and
+/// instructions per multiplication is needed on host side.
+#[derive(Clone, Copy, Debug)]
+pub struct Transform(__m128, __m128, __m128);
 impl Mul<Transform> for Transform {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self {
         unsafe {
-            let z = _mm_set_ps(1.0, 0.0, 0.0, 0.0);
-            let c1c2lo = _mm_unpacklo_ps(rhs.0, rhs.1);
-            let c1c2hi = _mm_unpacklo_ps(rhs.2, z);
-            let c3c4lo = _mm_unpackhi_ps(rhs.0, rhs.1);
-            let c3c4hi = _mm_unpackhi_ps(rhs.2, z);
-            let c1 = _mm_movelh_ps(c1c2lo, c1c2hi);
-            let c2 = _mm_movehl_ps(c1c2hi, c1c2lo);
-            let c3 = _mm_movelh_ps(c3c4lo, c3c4hi);
-            let c4 = _mm_movehl_ps(c3c4hi, c3c4lo);
-            let r1 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.0, c1, 0xf1), _mm_dp_ps(self.0, c2, 0xf2)), _mm_or_ps(_mm_dp_ps(self.0, c3, 0xf4), _mm_dp_ps(self.0, c4, 0xf8)));
-            let r2 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.1, c1, 0xf1), _mm_dp_ps(self.1, c2, 0xf2)), _mm_or_ps(_mm_dp_ps(self.1, c3, 0xf4), _mm_dp_ps(self.1, c4, 0xf8)));
-            let r3 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.2, c1, 0xf1), _mm_dp_ps(self.2, c2, 0xf2)), _mm_or_ps(_mm_dp_ps(self.2, c3, 0xf4), _mm_dp_ps(self.2, c4, 0xf8)));
+            let Matrix(x, y, z, w) = rhs.to_matrix();
+            let r1 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.0, x, 0xf1), _mm_dp_ps(self.0, y, 0xf2)), _mm_or_ps(_mm_dp_ps(self.0, z, 0xf4), _mm_dp_ps(self.0, w, 0xf8)));
+            let r2 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.1, x, 0xf1), _mm_dp_ps(self.1, y, 0xf2)), _mm_or_ps(_mm_dp_ps(self.1, z, 0xf4), _mm_dp_ps(self.1, w, 0xf8)));
+            let r3 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(self.2, x, 0xf1), _mm_dp_ps(self.2, y, 0xf2)), _mm_or_ps(_mm_dp_ps(self.2, z, 0xf4), _mm_dp_ps(self.2, w, 0xf8)));
             Transform(r1, r2, r3)
         }
     }
@@ -133,16 +143,39 @@ impl PartialEq for Transform {
             _mm_movemask_ps(_mm_cmpeq_ps(self.0, rhs.0)) & _mm_movemask_ps(_mm_cmpeq_ps(self.1, rhs.1)) & _mm_movemask_ps(_mm_cmpeq_ps(self.2, rhs.2)) == 0x0f
         }
     }
-}
-impl Transform {
-    pub fn new(x: Vector, y: Vector, z: Vector, shift: Vector) -> Transform {
+    fn ne(&self, rhs: &Transform) -> bool {
         unsafe {
-            let x = _mm_or_ps(x.0, _mm_shuffle_ps(shift.0, shift.0, 0x3f));
-            let y = _mm_or_ps(y.0, _mm_shuffle_ps(shift.0, shift.0, 0x7f));
-            let z = _mm_or_ps(z.0, _mm_shuffle_ps(shift.0, shift.0, 0xbf));
-            Transform(x, y, z)
+            _mm_movemask_ps(_mm_cmpneq_ps(self.0, rhs.0)) & _mm_movemask_ps(_mm_cmpneq_ps(self.1, rhs.1)) & _mm_movemask_ps(_mm_cmpneq_ps(self.2, rhs.2)) == 0x0f
         }
     }
+}
+impl Transform {
+    #[inline]
+    pub fn new(x: Vector, y: Vector, z: Vector, shift: Vector) -> Transform {
+        unsafe {
+            let r1r2lo = _mm_unpacklo_ps(x.0, y.0);
+            let r1r2hi = _mm_unpacklo_ps(z.0, shift.0);
+            let r3r4lo = _mm_unpackhi_ps(x.0, y.0);
+            let r3r4hi = _mm_unpackhi_ps(z.0, shift.0);
+            let r1 = _mm_movelh_ps(r1r2lo, r1r2hi);
+            let r2 = _mm_movehl_ps(r1r2hi, r1r2lo);
+            let r3 = _mm_movelh_ps(r3r4lo, r3r4hi);
+            Transform(r1, r2, r3)
+        }
+    }
+    /// Make a lookat matrix from a camera to a target to be looked at. The
+    /// camera is rolled away from the +y direction, by a positive angle counter-
+    /// clockwise or a negative angle counter-clockwise.
+    #[inline]
+    pub fn lookat(offset: Point, lookat: Point, roll: f32) -> Transform {
+        let (sin, cos) = roll.sin_cos();
+        let up = Vector::new(sin, cos, 0.0);
+        let n = (offset - lookat).sgn();
+        let u = up.cross(n).sgn();
+        let v = n.cross(u);
+        Transform::new(u, v, n, Point::default() - offset)
+    }
+    #[inline]
     pub fn eye() -> Transform {
         unsafe {
             Transform(
@@ -150,6 +183,22 @@ impl Transform {
                 _mm_set_ps(0.0, 0.0, 1.0, 0.0),
                 _mm_set_ps(0.0, 1.0, 0.0, 0.0),
             )
+        }
+    }
+}
+impl ToMatrix for Transform {
+    pub fn to_matrix(&self) -> Matrix {
+        unsafe {
+            let z = _mm_set_ps(1.0, 0.0, 0.0, 0.0);
+            let c1c2lo = _mm_unpacklo_ps(self.0, self.1);
+            let c1c2hi = _mm_unpacklo_ps(self.2, z);
+            let c3c4lo = _mm_unpackhi_ps(self.0, self.1);
+            let c3c4hi = _mm_unpackhi_ps(self.2, z);
+            let c1 = _mm_movelh_ps(c1c2lo, c1c2hi);
+            let c2 = _mm_movehl_ps(c1c2hi, c1c2lo);
+            let c3 = _mm_movelh_ps(c3c4lo, c3c4hi);
+            let c4 = _mm_movehl_ps(c3c4hi, c3c4lo);
+            Matrix(c1, c2, c3, c4)
         }
     }
 }
@@ -205,9 +254,9 @@ mod test {
     fn test_trans() {
         let eye = Transform::eye();
         let ones = Vector::new(1.0, 1.0, 1.0);
-        assert_eq!(eye * ones, ones);
+        assert_eq!(ones * eye, ones);
         let vec = Vector::new(1.0, 2.0, 3.0);
         let trans = Transform::new(vec, vec, vec, vec);
-        assert_eq!(trans * ones, ones * 6.0);
+        assert_eq!(ones * trans, Vector::new(4.0, 8.0, 12.0));
     }
 }
