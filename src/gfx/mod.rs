a5c67d9956759972d3b600b57ffdb5d0aceec312
PENGUINLIONG <admin@penguinliong.moe>
2019-11-11 17:53:50 -0800

    Spirv reflection: Material variable symbol

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -3,29 +3,11 @@ use std::result;
 mod error;
 mod ctxt;
 mod spv;
+mod sym;
 
 pub use error::*;
 pub use ctxt::*;
 pub use spv::*;
+pub use sym::*;
 
 pub type Result<T> = result::Result<T, Error>;
-
-/*
-pub struct RenderConfig {
-    Pipeline: Enumerate;
-}
-impl RenderConfig {
-    pub fn new() -> RenderConfig {
-        RenderConfig {
-            instance: instance
-        }
-    }
-}
-
-pub struct PipelineInterface {
-    pl: Pipeline;
-}
-impl PipelineInterface {
-
-}
-*/
